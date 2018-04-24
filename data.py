2bdaba65f32937cf75b84f758f6f47cdf2cd65e2
José Mena Roldan <jose.mena@eurecat.org>
2018-04-24 23:13:35 +0200

    JMR: adding pretrained embeddings

--- data.py
+++ data.py
@@ -3,23 +3,79 @@ from utils import *
 from keras.preprocessing import sequence
 from keras.preprocessing.sequence import skipgrams
 
+
 class bern_emb_data():
     def __init__(self, cs, ns, n_minibatch, L):
-        #assert cs%2 == 0
+        # assert cs % 2 == 0
         self.cs = cs
         self.ns = ns
         self.n_minibatch = n_minibatch
         self.L = L
-        #url = 'http://mattmahoney.net/dc/'
-        #filename = maybe_download(url, 'text8.zip', 31344016)
-        #filename = '/Users/jose.mena/Dropbox/Jose/PhD/data/recipes.txt.zip'
-        filename = '../data/wiki/wiki106.txt.zip'
+        filename = '/Users/jose.mena/dev/personal/data/wiki/wiki106.txt.zip'
         chars = read_data_as_chars(filename)
         self.build_dataset(chars)
         self.batch = self.batch_generator()
         self.N = len(self.data)
 
+    def build_dataset(self, words):
+        count = [['UNK', -1]]
+        count.extend(collections.Counter(words).most_common(self.L - 1))
+        dictionary = dict()
+        for word, _ in count:
+            dictionary[word] = len(dictionary)
+        data = list()
+        self.L = len(dictionary)
+        unk_count = 0
+        for word in words:
+            if word in dictionary:
+                index = dictionary[word]
+            else:
+                index = 0
+                unk_count += 1
+            data.append(index)
+        count[0][1] = unk_count
+        reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+        self.data = np.array(data)
+        self.count = count
+        self.dictionary = dictionary
+        self.labels = [reverse_dictionary[x] for x in range(len(reverse_dictionary))]
+        unigram_dist = np.array([1.0 * i for ii, i in count])
+        unigram_dist = (unigram_dist / unigram_dist.sum()) ** (3.0 / 4)
+        self.unigram = unigram_dist / unigram_dist.sum()
+        with open('fits/vocab.tsv', 'w') as txt:
+            for word in self.labels:
+                txt.write(word + '\n')
+
+    def batch_generator(self):
+        batch_size = self.n_minibatch + self.cs
+        data = self.data
+        while True:
+            if data.shape[0] < batch_size:
+                data = np.hstack([data, self.data])
+                if data.shape[0] < batch_size:
+                    continue
+            words = data[:batch_size]
+            data = data[batch_size:]
+            yield words
+
+    def feed(self, placeholder):
+        return {placeholder: self.batch.next()}
+
+class bayessian_bern_emb_data():
+    def __init__(self, cs, ns, n_minibatch, L, in_file):
+        # assert cs%2 == 0
+        self.cs = cs
+        self.ns = ns
+        self.n_minibatch = n_minibatch
+        self.L = L
+        self.in_file = in_file
+        chars = read_data_as_chars(self.in_file)
+        self.build_dataset(chars)
+        self.batch = self.batch_generator()
+        self.N = len(self.data)
+
     def build_dataset(self, chars):
+        test_split = 0.8
         count = [['UNK', -1]]
         count.extend(collections.Counter(chars).most_common(self.L - 1))
         dictionary = dict()
@@ -30,10 +86,10 @@ class bern_emb_data():
         unk_count = 0
         for char in chars:
             if char in dictionary:
-              index = dictionary[char]
+                index = dictionary[char]
             else:
-              index = 0 
-              unk_count += 1
+                index = 0
+                unk_count += 1
             data.append(index)
         count[0][1] = unk_count
         reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
@@ -42,31 +98,36 @@ class bern_emb_data():
         self.dictionary = dictionary
         self.chars = [reverse_dictionary[x] for x in range(len(reverse_dictionary))]
         sampling_table = sequence.make_sampling_table(len(dictionary))
-        couples, labels = skipgrams(data,
-                                    len(dictionary),
-                                    window_size=self.cs,
-                                    sampling_table=sampling_table,
-                                    negative_samples=self.ns)
+        couples, all_labels = skipgrams(data,
+                                        len(dictionary),
+                                        window_size=self.cs,
+                                        sampling_table=sampling_table,
+                                        negative_samples=self.ns)
         del data
-        self.labels = np.array(labels)
-        chars_target, chars_context = zip(*couples)
+        self.labels_train = np.array(all_labels[:int(len(all_labels) * test_split)])
+        self.labels_test = np.array(all_labels[int(len(all_labels) * test_split):len(all_labels)])
+        all_chars_target, all_chars_context = zip(*couples)
         del couples
-        self.chars_target = np.array(chars_target, dtype="int32")
-        self.chars_context = np.array(chars_context, dtype="int32")
+        self.chars_target_train = np.array(all_chars_target[:int(len(all_chars_target) * test_split)], dtype="int32")
+        self.chars_target_test = np.array(
+            all_chars_target[int(len(all_chars_target) * test_split):len(all_chars_target)], dtype="int32")
+        self.chars_context_train = np.array(all_chars_context[:int(len(all_chars_context) * test_split)], dtype="int32")
+        self.chars_context_test = np.array(
+            all_chars_context[int(len(all_chars_context) * test_split):len(all_chars_context)], dtype="int32")
         with open('fits/vocab.tsv', 'w') as txt:
             for char in self.chars:
-                txt.write(char+'\n')
+                txt.write(char + '\n')
 
     def batch_generator(self):
         batch_size = self.n_minibatch
-        data_target = self.chars_target
-        data_context = self.chars_context
-        data_labels = self.labels
+        data_target = self.chars_target_train
+        data_context = self.chars_context_train
+        data_labels = self.labels_train
         while True:
             if data_target.shape[0] < batch_size:
-                data_target = np.hstack([data_target, self.chars_target])
-                data_context = np.hstack([data_context, self.chars_context])
-                data_labels = np.hstack([data_labels, self.labels])
+                data_target = np.hstack([data_target, self.chars_target_train])
+                data_context = np.hstack([data_context, self.chars_context_train])
+                data_labels = np.hstack([data_labels, self.labels_train])
                 if data_target.shape[0] < batch_size:
                     continue
             chars_target = data_target[:batch_size]
@@ -76,12 +137,13 @@ class bern_emb_data():
             data_context = data_context[batch_size:]
             data_labels = data_labels[batch_size:]
             yield chars_target, chars_context, labels
-    
-    def feed(self, target_placeholder, context_placeholder, labels_placeholder, y_ph):
+
+    def feed(self, target_placeholder, context_placeholder, labels_placeholder,
+             ones_placeholder, zeros_placeholder):
         chars_target, chars_context, labels = self.batch.next()
         return {target_placeholder: chars_target,
                 context_placeholder: chars_context,
                 labels_placeholder: labels,
-                y_ph: np.ones((self.n_minibatch), dtype=np.int32)
+                ones_placeholder: np.ones((self.n_minibatch), dtype=np.int32),
+                zeros_placeholder: np.zeros((self.n_minibatch), dtype=np.int32)
                 }
-
--- main_bayesian.py
+++ main_bayesian.py
@@ -10,35 +10,45 @@ import tensorflow as tf
 from data import *
 from models import *
 from args import *
+from collections import Counter
+import pandas as pd
+import math
+import operator
 
 args, dir_name = parse_args()
 os.makedirs(dir_name)
 sess = ed.get_session()
 
 # DATA
-d = bern_emb_data(args.cs, args.ns, args.mb, args.L)
+d = bayessian_bern_emb_data(args.cs, args.ns, args.mb, args.L, args.in_file)
 
 # MODEL
-m = bayesian_emb_model(d, args.K, args.sig, sess, dir_name)
+m = bayesian_emb_model(d, args.K, sess, dir_name, args.emb_file)
 
 
 def get_n_iters():
-    n_batches = len(d.chars_target) / d.n_minibatch
-    if len(d.chars_target) % d.n_minibatch > 0:
+    n_batches = len(d.chars_target_train) / d.n_minibatch
+    if len(d.chars_target_train) % d.n_minibatch > 0:
         n_batches += 1
     return int(n_batches) * args.n_epochs, int(n_batches)
 
 
 # TRAINING
 n_iters, n_batches = get_n_iters()
-m.inference.initialize(n_samples=5, n_iter=n_iters, logdir=m.logdir)
+
+m.inference.initialize(n_samples=10, n_iter=n_iters, logdir=m.logdir,
+                       scale={m.y_pos: n_batches, m.y_neg: n_batches/args.ns},
+                       kl_scaling={m.y_pos: n_batches, m.y_neg: n_batches/args.ns}
+                      )
 init = tf.global_variables_initializer()
 sess.run(init)
 for i in range(m.inference.n_iter):
     info_dict = m.inference.update(feed_dict=d.feed(m.target_placeholder,
                                                     m.context_placeholder,
                                                     m.labels_placeholder,
-                                                    m.y_ph))
+                                                    m.ones_placeholder,
+                                                    m.zeros_placeholder
+                                                   ))
     m.inference.print_progress(info_dict)
     if i % n_batches == 0:
         m.saver.save(sess, os.path.join(m.logdir, "model.ckpt"), i)
@@ -46,6 +56,57 @@ m.saver.save(sess, os.path.join(m.logdir, "model.ckpt"), i)
 print('training finished. Results are saved in '+dir_name)
 m.dump(dir_name +"/variational.dat", d.chars, 100)
 
-y_posterior = ed.copy(m.y, {m.U: m.qU, m.V: m.qV})
+# Build samples from inferred posterior.
+n_samples = 500
+probs = []
+char_target = np.array([d.dictionary[' ']])
+char_context = np.array([d.dictionary['j']])
+for _ in range(n_samples):
+    target_embs = m.qU.sample()
+    rho = tf.nn.embedding_lookup(target_embs, char_target)
+    rho = (rho / tf.norm(rho))
+    context_embs = m.qV.sample()
+    alpha = tf.nn.embedding_lookup(context_embs, char_context)
+    alpha = (alpha / tf.norm(alpha))
+    prob = tf.reduce_sum(tf.multiply(rho, alpha), -1)
+    probs.append(tf.sigmoid(prob))
+
+outputs = tf.stack(probs).eval()
+outputs.mean()
+# outputs
+
+# sort by the sigmas
+sigmas = list(m.sigV.eval()[:,0])
+sorted_x = sorted(d.dictionary.items(), key=operator.itemgetter(1))
+dictionary_keys, _ = zip(*sorted_x)
+sigma_df = pd.DataFrame(
+    {'letter': dictionary_keys,
+     'sigma': sigmas
+    })
+list(sigma_df.letter)
+letters_by_sigma = sigma_df.sort_values(by='sigma')
+
+#sort by entropy
+filename = '/Users/jose.mena/dev/personal/data/wiki/wiki106.txt.zip'
+chars = read_data_as_chars(filename)
+count_chars = Counter(chars)
+count_pairs = Counter(zip(chars, chars[1:]))
+total_pairs = np.array(count_pairs.values()).sum()
+
+def weighted_information(value):
+    prob = 1. * value / total_pairs
+    return -((prob) * (math.log(prob)))
+
+values = count_pairs.values()
+first_letters, second_letters = zip(*count_pairs.keys())
+pairs_df = pd.DataFrame(
+    {'first_letter': first_letters,
+     'second_letter': second_letters,
+     'occurs': values
+    })
+pairs_df['entropy'] = pairs_df['occurs'].apply(weighted_information)
+entropy = pairs_df.groupby('first_letter').sum()
+letters_by_entropy = entropy.sort_values(by='entropy', ascending=False)
+letters_by_entropy.index.name = None
 
 print('Done')
